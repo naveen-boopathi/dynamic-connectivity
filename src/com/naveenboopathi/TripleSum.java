c4969b0b8fc9be420a3e88d45cd9482900caa495
Naveen Boopathi <naveenboopathi18@outlook.com>
2023-10-10 10:18:18 +0530

    Add triple sum algorithm

---
+++ src/com/naveenboopathi/TripleSum.java
@@ -0,0 +1,23 @@
+package com.naveenboopathi;
+
+public class TripleSum {
+    public static int regularCount(int[] a) {
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if(a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {30, -40, -20, -10, 40, 0, 10, 5};
+        System.out.println(regularCount(a));
+    }
+}
